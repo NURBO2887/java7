91a3846def8d22a319493a4575e52e31ca94ea2e
NURBO2887 <nurbo2887@gmail.com>
2022-11-07 10:36:18 +0600

    приступит к проекту

---
+++ main.js
@@ -0,0 +1,11 @@
+const TodoListTask =[];
+var TodoLisAddBtn = document.getElementById
+('todo-list-task-add-btn')
+TodoLisAddBtn.addEventListener('click', AddTodo)
+function AddTodo(){
+    var todoListText = document.getElementById('todo-list-task-text').value;
+    console.log(todoListText)
+}
+   
+
+
